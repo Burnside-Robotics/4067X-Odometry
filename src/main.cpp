c78be36f689dd3f9b2a0a42dc195e7e395840db0
Alex Cutforth <alex@densem.net>
2021-04-14 10:11:23 +1200

    Added Drive Control

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,10 @@ motor lDrive1(PORT1, ratio18_1);
 motor lDrive2(PORT2, ratio18_1);
 motor rDrive1(PORT3, ratio18_1);
 motor rDrive2(PORT4, ratio18_1);
+motor rDrive1(PORT3, ratio18_1, true);
+motor rDrive2(PORT4, ratio18_1, true);
 
+inertial inertialSensor(PORT10);
 
 
 const float encoderWheelCircumfrence = 0;
@@ -52,7 +55,87 @@ int previousLEncoder = 0;
 int previousREncoder = 0;
 int previousBackEncoder = 0;
 
+float MOTOR_ACCEL_LIMIT = 10;
 
+
+int lastl1Speed = 0;
+int lastl2Speed = 0;
+int lastr1Speed = 0;
+int lastr2Speed = 0;
+
+float toRadians(float input)
+{
+  return input / (180 / 3.1415926535);
+}
+float toDegrees(float input)
+{
+  return input * (180 / 3.1415926535);
+}
+// Adjusts Input by robot heading
+void AdjustToRotation(int& forwardAxis, int& lateralAxis)
+{
+  int polarRadius = sqrt(lateralAxis * lateralAxis + forwardAxis * forwardAxis);
+  float theta = toDegrees((atan(forwardAxis / lateralAxis)));
+  int head = inertialSensor.heading();
+  if(lateralAxis < 0)
+    theta += 180;
+  else if(forwardAxis < 0)
+    theta += 360;
+
+  if(head > 180)
+    head -= 360;
+  theta += head;
+  forwardAxis = polarRadius * sin(toRadians(theta));
+  lateralAxis = polarRadius * cos(toRadians(theta)); 
+}
+void DriveWheels(int forwardAxis, int lateralAxis, int rotalAxis)
+{
+  AdjustToRotation(forwardAxis, lateralAxis);
+  int l1Speed = forwardAxis + lateralAxis + rotalAxis;
+  int l2Speed = forwardAxis - lateralAxis + rotalAxis;
+  int r1Speed = forwardAxis - lateralAxis - rotalAxis;
+  int r2Speed = forwardAxis + lateralAxis - rotalAxis;
+
+  if ((l1Speed - lastl1Speed) > MOTOR_ACCEL_LIMIT)
+      l1Speed = lastl1Speed + MOTOR_ACCEL_LIMIT;
+  if ((l1Speed - lastl1Speed) < -MOTOR_ACCEL_LIMIT)
+      l1Speed = lastl1Speed - MOTOR_ACCEL_LIMIT;
+  if ((l2Speed - lastl2Speed) > MOTOR_ACCEL_LIMIT)
+      l2Speed = lastl2Speed + MOTOR_ACCEL_LIMIT;
+  if ((l2Speed - lastl2Speed) < -MOTOR_ACCEL_LIMIT)
+      l2Speed = lastl2Speed - MOTOR_ACCEL_LIMIT;
+
+  if ((r1Speed - lastr1Speed) > MOTOR_ACCEL_LIMIT)
+      r1Speed = lastr1Speed + MOTOR_ACCEL_LIMIT;
+  if ((r1Speed - lastr1Speed) < -MOTOR_ACCEL_LIMIT)
+      r1Speed = lastr1Speed - MOTOR_ACCEL_LIMIT;
+  if ((r2Speed - lastr2Speed) > MOTOR_ACCEL_LIMIT)
+      r2Speed = lastr2Speed + MOTOR_ACCEL_LIMIT;
+  if ((r2Speed - lastr2Speed) < -MOTOR_ACCEL_LIMIT)
+      r2Speed = lastr2Speed - MOTOR_ACCEL_LIMIT;
+
+  lastl1Speed = l1Speed;
+  lastl2Speed = l2Speed;
+  lastr1Speed = r1Speed;
+  lastr2Speed = r2Speed;
+
+  if (l1Speed == 0)
+      lDrive1.stop(brakeType::brake);
+  else
+      lDrive1.spin(directionType::fwd, l1Speed, velocityUnits::pct);
+  if (l2Speed == 0)
+      lDrive2.stop(brakeType::brake);
+  else
+      lDrive2.spin(directionType::fwd, l2Speed, velocityUnits::pct);
+  if (r1Speed == 0)
+      rDrive1.stop(brakeType::brake);
+  else
+      rDrive1.spin(directionType::fwd, r1Speed, velocityUnits::pct);
+  if (r2Speed == 0)
+      rDrive2.stop(brakeType::brake);
+  else
+      rDrive2.spin(directionType::fwd, r2Speed, velocityUnits::pct);
+}
 void CalculatePosition()
 {
   float lDistChange = (lEncoder.rotation(deg) - previousLEncoder) / 360 * encoderWheelCircumfrence;
@@ -91,9 +174,33 @@ void autonomous(void) {
 
 }
 
+void HandleDriveInput()
+{
+  int forwardAxis = Controller1.Axis3.position();
+  int lateralAxis = Controller1.Axis4.position();
+  int rotalAxis = Controller1.Axis1.position();
 
+  if(Controller1.ButtonUp.pressing())
+  {
+    forwardAxis += 100;
+  }
+  else if(Controller1.ButtonDown.pressing())
+  {
+    forwardAxis -= 100;
+  }
+  else if(Controller1.ButtonRight.pressing())
+  {
+    lateralAxis += 100;
+  }
+  else if(Controller1.ButtonLeft.pressing())
+  {
+    lateralAxis -= 100;
+  }
+  DriveWheels(forwardAxis, lateralAxis, rotalAxis);
+}
 void usercontrol(void) {
   while (1) {
+    HandleDriveInput();
     CalculatePosition();
     wait(20, msec);
   }
